8e44b447764ba321e12ea78be870f915ffaa59c2
Invictus Navarchus <github.com.compile095@passmail.com>
2025-12-23 05:56:35 +0700

    chore: add gecko id and strict min version for firefox manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,4 +4,12 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
     srcDir: 'src',
     modules: ['@wxt-dev/auto-icons'],
+    manifest: {
+    browser_specific_settings: {
+      gecko: {
+        id: "udemy-show-release-date@InvictusNavarchus",
+        strict_min_version: "109.0" // MV3 support started in Firefox 109
+      }
+    }
+  }
 });
