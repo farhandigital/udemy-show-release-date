c5d7ecbcb3f1efb7c90cf4918d03d038e2d994bc
Invictus Navarchus <github.com.compile095@passmail.com>
2025-12-22 19:55:28 +0700

    build: add and enable @wxt-dev/auto-icons module

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-    srcDir: 'src'
+    srcDir: 'src',
+    modules: ['@wxt-dev/auto-icons'],
 });
