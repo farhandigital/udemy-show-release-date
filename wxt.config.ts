1d40a8f393ee51a11c035eb21d22a472c8c896ad
Invictus Navarchus <github.com.compile095@passmail.com>
2025-12-23 10:05:45 +0700

    chore: add data_collection_permissions to Firefox manifest settings to comply with latest AMO policy

--- wxt.config.ts
+++ wxt.config.ts
@@ -8,7 +8,11 @@ export default defineConfig({
     browser_specific_settings: {
       gecko: {
         id: "udemy-show-release-date@InvictusNavarchus",
-        strict_min_version: "109.0" // MV3 support started in Firefox 109
+        // @ts-expect-error - data_collection_permissions is valid Firefox manifest setting
+        data_collection_permissions: {
+          "required": ["none"]
+        },
+        strict_min_version: "109.0", // MV3 support started in Firefox 109
       }
     }
   }
